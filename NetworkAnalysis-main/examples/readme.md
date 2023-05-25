efc7905e48addb243dd839f3540670d80e2dc22b
Pieter-Paul Strybol <34382674+pstrybol@users.noreply.github.com>
2023-05-25 09:25:47 +0200

    Delete NetworkAnalysis-main directory

--- NetworkAnalysis-main/examples/readme.md
+++
@@ -1,2 +0,0 @@
-# Examples of how to use the different classes.
-All examples are tested to run without errors, so if you run into one, check that you have the latest version of classes.
--- NetworkAnalysis-main/getCoverage.sh
+++
@@ -1,6 +0,0 @@
-cd pytests/
-coverage run --omit="*/OpenNE*" -m pytest
-coverage html --omit="*/test*,*/OpenNE*"
-mkdir -p Coverage
-rm Coverage/coverage.svg
-coverage-badge -o Coverage/coverage.svg
